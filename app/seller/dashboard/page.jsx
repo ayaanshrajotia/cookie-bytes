54f353d24971c4c9a915b0f20a06d4b4e94d10ec
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2023-09-06 23:09:05 +0530

    basic layout creation

---
+++ app/seller/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SellerDashboard = () => {
+    return (
+        <div className="flex flex-col items-center justify-between">
+            Seller Dashboard
+        </div>
+    );
+};
+
+export default SellerDashboard;
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="flex flex-col items-center justify-between">Footer</div>
+    );
+};
+
+export default Footer;
